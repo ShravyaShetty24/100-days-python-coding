8b2f662f895c12b30a74b108e39a76665004addf
ShravyaShetty24 <shravyashetty022007@gmail.com>
2026-05-05 23:46:59 +0530

    Add files via upload
    Day 7/100

---
+++ Day7.py
@@ -0,0 +1,16 @@
+#building a guesing game
+secret_word="engineering"
+guess=" "
+guess_count=0
+guess_limit=3
+out_of_guess=False
+while guess!=secret_word and not(out_of_guess):
+    if guess_count<guess_limit:
+        guess=input("Enter your guess:")
+        guess_count+=1
+    else:
+        out_of_guess=True
+if out_of_guess:
+    print("out of guess,you lose!")
+else:
+    print("you win!")
