f2567b735f993f922e49a503279ce476ea8ae4e5
ShravyaShetty24 <shravyashetty022007@gmail.com>
2026-05-10 22:13:43 +0530

    Add files via upload
    Day 11/100

---
+++ DAy11.py
@@ -0,0 +1,8 @@
+student={"name":"Shravya","age":"19","course":"python"}
+print(student)
+print(student["name"])
+student["city"]="Thirthahalli"
+student["age"]="20"
+print(student)
+student.pop("course")
+print(student)
