0c9812cfbf30f1b7bb7f1ece00ff557d66558a0e
ShravyaShetty24 <shravyashetty022007@gmail.com>
2026-07-30 20:21:01 +0530

    Add files via upload
    Day 93/100

---
+++ Day93.py
@@ -0,0 +1,8 @@
+#Manual Iteration
+movies=["KGF","Kantara","Charlie 777","Googly","Moggina manasu"]
+it=iter(movies)
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
